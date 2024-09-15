35dbf696ee66f93f9eb47f1ea1c24902ddf7833f
Elliot Ramirez <elliotfrg25@yahoo.com>
2024-09-15 13:25:44 -0500

    Optimización de Webpack y mejoras en el tamaño de archivos

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const TerserPlugin = require('terser-webpack-plugin'); // Asegúrate de requerirlo aquí
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // Archivos JavaScript y JSX
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Resuelve tanto .js como .jsx
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: true,
+        }),
+    ],
+};
+
